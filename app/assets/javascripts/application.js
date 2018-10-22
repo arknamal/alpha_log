c19a58abec244e06053a90b1d3d1089f828ca2af
ARK <abdulrehman@deltalabs.io>
2018-10-22 15:05:25 +0500

    Install Bootstrap remove typos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery-ujs
-//= require bootsrap-sprockets
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
